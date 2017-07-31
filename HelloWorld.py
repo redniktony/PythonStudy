e4b93082ccb32adce6da0e4953a428abc7d3c6ae
Tony <rednik111@gmail.com>
2017-08-01 01:12:42 +0800

    Add Assignment2

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,9 @@
+#quote test this is how to quote
 print('Hello World!')
+print('CIS 41- Assignment 2')
+print('XIA HUA')
+print('One of my favorite songs is Smoke Weed Everyday')
+print('\t yeyayay')
+print('\t heyhey')
+print('\t gaygay')
+print('\t thug life')
